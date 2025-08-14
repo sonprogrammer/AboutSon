6b305d51f4a22f57e3dce9b48f17eae449f13315
sonprogrammer <ods04138@gmail.com>
2025-08-14 22:20:49 +0900

    Stop tracking data folder

--- src/data/featureData.ts
+++
@@ -1,8 +0,0 @@
-export const featureData = [
-    {title: '한서대학교', subtitle: '졸업', detail: '항공소프트웨어공학과', img: 'schoolLogo.png'},
-    {title: '정보처리기사', subtitle: '취득(2024.09)', detail: '', img: 'certification.svg'},
-    {title: 'GTQ 1급', subtitle: '취득(2021.10)', detail: '', img: 'gtq1.svg'},
-    {title: '토익', subtitle: '835(2023.08)', detail: '', img: 'toeic.svg', thirdImg: 'score.svg'},
-    {title: '해외경험', subtitle: '뉴질랜드 워킹홀리데이 (2017.07 ~ 2018.07)', detail: '', img: 'newzealand.jpg'},
-    
-]
